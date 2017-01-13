2dc11547fc02a008fabfbfb45b68c911c04c66b6
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-01-14 01:38:34 +0800

    try out the output design in app

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,13 @@
 
+use output::{StdoutWriter, Writer};
+
 #[derive(Debug)]
 pub enum Error {
     Todo,
 }
 
 pub fn run() -> Result<(), Error> {
+    StdoutWriter::default().write("Hello world");
     Err(Error::Todo)
 }
 
