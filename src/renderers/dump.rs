7010784da2e9bc545c3647b13f282da4f18c09f0
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-01-24 22:02:35 +0800

    commit: better dump renderer
    shows the tree structure

--- src/renderers/dump.rs
+++ src/renderers/dump.rs
@@ -1,12 +1,52 @@
 use super::renderer;
-use fastup::Document;
+use fastup::{Document, Node};
+use fastup::Node::*;
 
 #[derive(Debug, Copy, Clone, Default)]
 pub struct Renderer;
 
 impl renderer::Renderer for Renderer {
     fn render(&self, doc: &Document) -> String {
-        format!("{:?}", doc)
+        dump_from_document(doc, "")
+    }
+}
+
+fn dump_from_document(doc: &Document, indent: &str) -> String {
+    doc.0
+        .iter()
+        .map(|node| dump_from_node(node, indent))
+        .collect::<Vec<_>>()
+        .join(&format!("\n{}", indent))
+}
+
+const INDENTATION: &'static str = "    ";
+
+fn dump_from_document_followed(doc: &Document, indent: &str) -> String {
+    assert!(doc.0.len() > 0);
+
+    if doc.0.len() == 1 {
+        " ".to_string() + &dump_from_document(doc, indent)
+    } else {
+        let indent = format!("{}{}", INDENTATION, indent);
+        format!(":\n{}", indent) + &dump_from_document(doc, &indent)
+    }
+}
+
+fn dump_from_node(node: &Node, indent: &str) -> String {
+    match *node {
+        Text(ref text) => format!("{:?}", text),
+        Foreground(color, ref doc) => {
+            format!("({}){}",
+                    color.clamp_to_888(),
+                    dump_from_document_followed(doc, indent))
+        }
+        Background(color, ref doc) => {
+            format!("[{}]{}",
+                    color.clamp_to_888(),
+                    dump_from_document_followed(doc, indent))
+        }
+        Bold(ref doc) => format!("{{}}{}", dump_from_document_followed(doc, indent)),
+        Widget(..) => unreachable!(),
     }
 }
 
