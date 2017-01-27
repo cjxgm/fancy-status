998652f66172d60d9f0c84e75e6ee1818be76927
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-01-27 20:56:36 +0800

    widget: cpu: use actual cpu data

--- src/widgets/cpu.rs
+++ src/widgets/cpu.rs
@@ -1,6 +1,9 @@
 use color::{Color888, Colorf32};
 use fastup::{self, Node, parse_for_first_node, parse_color};
+use sysinfo::System;
 use super::error_node;
+use std::time::Duration;
+use std::thread::sleep;
 
 #[derive(Debug, Copy, Clone, Default)]
 pub struct Widget;
@@ -22,7 +25,7 @@ fn try_node_from_args(args: Vec<String>) -> Result<Node, String> {
     assert_eq!(args.len(), 2);
 
     let cold = parse_color(&args[0]).map_err(escape_fastup)?;
-    let hot  = parse_color(&args[1]).map_err(escape_fastup)?;
+    let hot = parse_color(&args[1]).map_err(escape_fastup)?;
 
     Ok(cpu_node(cold.into(), hot.into()))
 }
@@ -34,12 +37,7 @@ fn cpu_node(cold: Colorf32, hot: Colorf32) -> Node {
     const IDLE_THRESHOLD: f32 = 0.05;
     const PROGRESS_INDICATOR: &'static str = "▁▂▃▄▅▆▇";
 
-    let cpu_usages = cpu_usages();
-    let cpu_total = {
-        let sum: f32 = cpu_usages.iter().sum();
-        let len = cpu_usages.len() as f32;
-        sum / len
-    };
+    let (cpu_total, cpu_usages) = cpu_usages();
 
     let bg_cold = cold.set_lightness(BG_LIGHTNESS);
     let bg_hot = hot.set_lightness(BG_LIGHTNESS);
@@ -65,11 +63,8 @@ fn cpu_node(cold: Colorf32, hot: Colorf32) -> Node {
         PROGRESS_INDICATOR.chars().nth(i).unwrap()
     };
 
-    let cpu_usages = cpu_usages
-        .into_iter()
-        .map(|usage| format!("({}:{})",
-                make_fg_color(usage),
-                make_cpu_indicator(usage)))
+    let cpu_usages = cpu_usages.into_iter()
+        .map(|usage| format!("({}:{})", make_fg_color(usage), make_cpu_indicator(usage)))
         .collect::<String>();
     let total_usage = {
         let color = make_fg_color(cpu_total);
@@ -83,8 +78,20 @@ fn cpu_node(cold: Colorf32, hot: Colorf32) -> Node {
     parse_for_first_node(&node).unwrap()
 }
 
-fn cpu_usages() -> Vec<f32> {
-    vec![0.0, 0.05, 0.1, 0.15, 0.3, 0.5, 0.7, 0.9, 1.0, 0.05]
+/// return (total, [usage])
+fn cpu_usages() -> (f32, Vec<f32>) {
+    let mut sys = System::new();
+    // FIXME: Dirty hack by sleeping.
+    // System refreshing should be done globally.
+    sleep(Duration::from_millis(100));
+    sys.refresh_system();
+    let p = sys.get_processor_list();
+    let total = p[0].get_cpu_usage();
+    let usages = p.iter()
+        .skip(1)
+        .map(|x| x.get_cpu_usage())
+        .collect::<Vec<_>>();
+    (total, usages)
 }
 
 fn escape_fastup<T>(input: T) -> String
