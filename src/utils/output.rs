5cb292addc1d77a93c5234561c3684ad006347af
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-01-28 23:51:58 +0800

    fancy_status::utils: use Write trait inside errln! macro

--- src/utils/output.rs
+++ src/utils/output.rs
@@ -1,11 +1,10 @@
 //! # Output utilities
 
-pub use std::io::Write;
-
 /// Similar to `println!(...)`, but write to `stderr` instead of `stdout`.
 #[macro_export]
 macro_rules! errln {
     ($($x:expr),*) => {
+        use std::io::Write;
         writeln!(::std::io::stderr(), $($x,)*).unwrap();
     };
 }
