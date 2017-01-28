d743ca1310a14d539fa24f987ae2171ad231cc6e
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-01-28 23:52:35 +0800

    refactor the entry code

--- src/app.rs
+++ src/app.rs
@@ -2,17 +2,17 @@
 use fast::output::{StdoutWriter, Writer};
 use fast::render_status;
 
+pub type Result<T> = ::std::result::Result<T, String>;
+
 #[derive(Debug)]
-pub enum Error {
-    Todo,
+pub struct Configuration {
+    pub expression: String,
+    pub renderer_name: String,
 }
 
-pub fn run() -> Result<(), Error> {
-    StdoutWriter::default().write("Hello world");
-    StdoutWriter::default().write(&render_status("hello <time> {world}", "dump").unwrap());
-    StdoutWriter::default().write(&render_status("hello <time> {world}", "ansi").unwrap());
-    StdoutWriter::default().write(&render_status("<gradient|50|ff0000|00ffff>", "ansi").unwrap());
-    StdoutWriter::default().write(&render_status("<cpu|00ff88|ff3322>", "ansi").unwrap());
-    Err(Error::Todo)
+pub fn run(config: &Configuration) -> Result<()> {
+    let status = render_status(&config.expression, &config.renderer_name)?;
+    StdoutWriter::default().write(&status);
+    Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@
 //!
 //! Gather and parse command line arguments and configuration files, pass
 //! them into the application and do some post-exit clean up.
+//!
+//! ## Example Fastup Expressions
+//!
+//! ```fastup
+//! hello <time> {world}
+//! <gradient|50|ff0000|00ffff>
+//! <cpu|00ff88|ff3322>
+//! ```
 
 #[macro_use]
 extern crate clap;
@@ -10,25 +18,28 @@ extern crate fancy_status as fast;
 
 mod app;
 
-use fast::utils::*;
-use std::process;
+use std::process::exit;
+use app::{Configuration, run};
 
 fn main() {
-    parse_args();
-    if let Err(e) = app::run() {
-        errln!("Error: {:?}", e);
-        process::exit(1);
+    let config = parse_args();
+    if let Err(err) = run(&config) {
+        errln!("{}", err);
+        exit(1);
     }
 }
 
-fn parse_args() {
+fn parse_args() -> Configuration {
     let args = clap_app! {
         @app (app_from_crate!())
-        (@arg EXPRESSION: -e --expression +takes_value "Evaluates EXPRESSION")
-        (@arg FILE: "Reads EXPRESSION from FILE and evaluates it.")
-        (@arg format: -f --format +takes_value default_value("ansi") possible_values(&["ansi", "tmux"]) "Sets output format")
-        (@arg debug: -d ... "Sets the level of debugging information")
-        (@arg verbose: -v ... "Sets the level of verbosity")
+        (@arg EXPRESSION: default_value("<time>") "Evaluates Fastup EXPRESSION if no <FILE> is given")
+        (@arg FILE: -f --file +takes_value "Reads Fastup EXPRESSION from FILE and evaluates it")
+        (@arg format: -o --output +takes_value default_value("ansi") possible_values(&["ansi", "tmux", "html", "dump"]) "Sets output format")
     }.get_matches();
+
+    Configuration {
+        expression: args.value_of("EXPRESSION").unwrap().into(), // TODO: read file
+        renderer_name: args.value_of("format").unwrap().into(),
+    }
 }
 
