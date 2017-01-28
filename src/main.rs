858e8ed8095e815b78ce32c69f01b7a16a0a0528
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-01-29 00:41:53 +0800

    main: argument parser now reads file

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,26 @@ extern crate fancy_status as fast;
 mod app;
 
 use std::process::exit;
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
 use app::{Configuration, run};
 
+type Result<T> = ::std::result::Result<T, String>;
+
 fn main() {
-    let config = parse_args();
-    if let Err(err) = run(&config) {
+    if let Err(err) = try_main() {
         errln!("{}", err);
         exit(1);
     }
 }
 
-fn parse_args() -> Configuration {
+fn try_main() -> Result<()> {
+    let config = parse_args()?;
+    run(&config)
+}
+
+fn parse_args() -> Result<Configuration> {
     let args = clap_app! {
         @app (app_from_crate!())
         (@arg EXPRESSION: default_value("<time>") "Evaluates Fastup EXPRESSION if no <FILE> is given")
@@ -37,9 +46,23 @@ fn parse_args() -> Configuration {
         (@arg format: -o --output +takes_value default_value("ansi") possible_values(&["ansi", "tmux", "html", "dump"]) "Sets output format")
     }.get_matches();
 
-    Configuration {
-        expression: args.value_of("EXPRESSION").unwrap().into(), // TODO: read file
-        renderer_name: args.value_of("format").unwrap().into(),
+    Ok(Configuration {
+        expression: match args.value_of("FILE") {
+            Some(path) => read_whole_file(path)?,
+            None => args.value_of("EXPRESSION").unwrap().into(),    // `EXPRESSION` has default value, just unwrap
+        },
+        renderer_name: args.value_of("format").unwrap().into(),     // `format` has default value, just unwrap
+    })
+}
+
+fn read_whole_file<T>(path: T) -> Result<String>
+    where T: AsRef<Path>
+{
+    let mut content = String::new();
+    {
+        let mut file = File::open(path).map_err(|e| e.to_string())?;
+        file.read_to_string(&mut content).map_err(|e| e.to_string())?;
     }
+    Ok(content)
 }
 
