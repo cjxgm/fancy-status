0cd50c9a61e39ca0c02c13434d231c892fbf1861
Giumo X. Clanjor (哆啦比猫/兰威举) <cjxgm@126.com>
2017-01-31 19:34:33 +0800

    stats-once: gather memory usage

---
+++ stats-once/src/mem.rs
@@ -0,0 +1,25 @@
+use system::SYSTEM;
+
+lazy_static! {
+    /// ```rust
+    /// # use stats_once::mem::*;
+    /// assert!(*TOTAL >= 0);
+    /// ```
+    pub static ref TOTAL: i32 = {
+        mib_from_kib(SYSTEM.get_total_memory())
+    };
+
+    /// ```rust
+    /// # use stats_once::mem::*;
+    /// assert!(*USED >= 0);
+    /// ```
+    pub static ref USED: i32 = {
+        mib_from_kib(SYSTEM.get_used_memory())
+    };
+}
+
+/// Convert byte size from KiB to MiB.
+fn mib_from_kib(x: u64) -> i32 {
+    (x / (1 << 10)) as i32
+}
+
